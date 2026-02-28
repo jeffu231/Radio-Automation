6ff115858e090d633c4e15d51de9d39f6c615ca2
Jeff Uchitjil <git@k9kld.org>
2026-02-28 12:50:02 -0600

    ci(Installer): Create a simple installer
    * Setup a basic installer based on WIX.
    * Add a Windows Sanbox configuration to enable testing the install in isolation.

---
+++ sandbox/scripts/startup.bat
@@ -0,0 +1,20 @@
+REM Download windowsdesktop-runtime
+curl -L "https://builds.dotnet.microsoft.com/dotnet/WindowsDesktop/10.0.3/windowsdesktop-runtime-10.0.3-win-x64.exe" --output C:\users\WDAGUtilityAccount\Downloads\windowsdesktop-runtime.exe
+
+REM Install windowsdesktop-runtime
+C:\users\WDAGUtilityAccount\Downloads\windowsdesktop-runtime.exe /norestart /quiet
+
+REM Download aspcore-runtime
+REM curl -L "https://builds.dotnet.microsoft.com/dotnet/aspnetcore/Runtime/10.0.3/aspnetcore-runtime-10.0.3-win-x64.exe" --output C:\users\WDAGUtilityAccount\Downloads\aspcore-runtime.exe
+
+REM Install aspcore-runtime
+REM C:\users\WDAGUtilityAccount\Downloads\aspcore-runtime.exe /norestart /quiet
+
+REM Install VC Redist
+REM C:\Dev\Vixen\Installer\Redist\VC_redist.x64.exe /install /passive /norestart
+
+REM Launch App install panel
+appwiz.cpl
+
+REM open output folder
+explorer "c:\Dev\Radio-Automation"
